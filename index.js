85ea9d4f42440beefac89c0df4bc0359759c64a1
BrocksPaulo <brockspaulo@protonmail.com>
2022-01-12 23:56:45 -0300

    Primeiras rotas

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
-
 const app = express();
+app.use(express.json());
 
-app.get('/', (request, response) => {
+app.get('/users', (request, response) => {   
+
     return response.json({
-        nome: 'Paulo',
-        sobrenome: 'Brocks'
+        nome: 'Xao',
+        sobrenome: 'Lolo'
     });
+});
+
+app.get('/filtranome', (request, response) => { 
+    const params = request.query;
+    
+    console.log(params);
+});
+
+app.get('/filtraid/:id', (request, response) => { 
+    const id = request.params;
+    
+    console.log(id);
+});
+
+app.post('/gravanome', (request, response) => { 
+    const body = request.body;
+    
+    console.log(body);
 });
 
 app.listen(3333);
